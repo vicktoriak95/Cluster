7a7d0eb5c1dfc52577959d86e9ac41927976dcac
antebi-itai <antebi.itai@gmail.com>
2020-08-08 22:17:02 +0300

    fix minor bugs in network.c

--- Network.c
+++ Network.c
@@ -10,14 +10,14 @@ Network* allocate_network(int n){
 	spmat* A = NULL;
 	int* deg_vector = NULL;
 
-	/* allocate the network */
+	/* Allocate the network */
 	net = (Network*)allocate(sizeof(Network));
 
-	/* allocate the adjacency matrix */
+	/* Allocate the adjacency matrix */
 	A = spmat_allocate(n);
 	net->A = A;
 
-	/* allocate the degrees vector */
+	/* Allocate the degrees vector */
 	deg_vector = (int*)allocate(sizeof(int) * n);
 	net->deg_vector = deg_vector;
 
@@ -27,32 +27,28 @@ Network* allocate_network(int n){
 Network* create_network(FILE* input){
 	Network* net;
 	int n = 0;
-	//int M = 0;
+	int M = 0;
 	int* row = NULL;
 	int i = 0;
 	int j = 0;
-	int next_adj_edge = 0;
-	int row_index = 0;
 
-	/* read n from the file */
+	/* Read n from the file */
 	n = int_fread(input);
 
-	/* allocate the network and its members */
+	/* Allocate the network and its members */
 	net = allocate_network(n);
 	net->n = n;
 
-	/* allocate space for a row of the adjacency matrix */
+	/* Allocate space for a row of the adjacency matrix */
 	row = (int*)malloc(sizeof(int) * net->n);
 
-	/* */
+	/* Update the degrees vector and adjacency matrix from the file*/
 	for(i = 0; i < net->n; i++){
-		net->deg_vector[i] = int_fread(input);
-		for (j = 0; j < net->deg_vector[i]; j ++){
-			//TODO: finish this! add row to sparse matrix !
-
-		}
-		spmat_add_row(net->A, row, i);
+		net->deg_vector[i] = spmat_add_row_from_file(net->A, input, i);
 	}
+
+	/* Calculate the sum of all degrees in network */
+	M = sum_of_vector(net->deg_vector, net->n);
 
 	return net;
 }
