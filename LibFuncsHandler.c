15452b9d8f23fb49c906788e6886011ec391af19
antebi-itai <antebi.itai@gmail.com>
2020-08-08 21:44:28 +0300

    Create network.c (undone) and LibFuncsHandler

---
+++ LibFuncsHandler.c
@@ -0,0 +1,33 @@
+/*
+ * LibFuncsHandler.c
+ *
+ *  Created on: 8 баев„ 2020
+ *      Author: user
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int int_fread(FILE* f){
+	int n = 0;
+	int return_value = 0;
+
+	/* read int from the file */
+	return_value = fread(&n, sizeof(int), 1, f);
+	if (return_value != sizeof(int)) {
+		printf("File reading failed");
+		exit(1);
+	}
+
+	return n;
+}
+
+void* allocate(size_t n){
+	void* p = NULL;
+	p = malloc(n);
+	if (p == NULL){
+		printf("Memory allocation failed");
+		exit(1);
+	}
+	return p;
+}
